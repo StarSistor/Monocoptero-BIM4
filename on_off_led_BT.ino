9b63cea42a26ffecb69dc12c4a77cfe216381dbd
StarSistor <114177409+StarSistor@users.noreply.github.com>
2022-11-19 14:47:30 -0500

    Update on_off_led_BT.ino

--- on_off_led_BT.ino
+++ on_off_led_BT.ino
@@ -1,24 +1,33 @@
-/* Control de encendido y apagado de lampara vía Bluetooth
- Se envía vía bluetooth a la tarjeta Arduino la letra a para activar el led del pin13 y la letra b para apaga
- */
-#include <SoftwareSerial.h>
-int dato;                   //Se crea la variable dato tipo entero
- 
-void setup() {                
-  pinMode(13, OUTPUT);     //Definimos el pin 13 como salida
-  Serial.begin(9600);      //Inicializamos el puerto serial
+#include <SoftwareSerial.h> // libreria que permite establecer pines digitales
+
+SoftwareSerial BT(10, 11);  // pin 10 como RX, pin 11 como TX
+
+char valor = 0;      // variable para almacenar caracter recibido
+
+void setup(){
+  BT.begin(9600);    // comunicacion serie entre Arduino y el modulo
+  Serial.begin(115200);
+pinMode(13, OUTPUT);
+
 }
-void loop() {
- if (Serial.available()>0) {       //Confirmamos si existe un valor en el puerto serie
-    dato = Serial.read();          //leemos el valor y lo asignamos a la variable dato
-     switch(dato)                    //comparamos el valor guardado en la variable dato
-     {
-       case 'a':                        //si el dato leido es a
-              digitalWrite(13,1);               //Setiamos a 5V el pin 13
-              break;
-      case 'b':                        //si el dato leido es b
-             digitalWrite(13,0);              //setiamos a 0V el pin 13
-             break;
-     }
+
+void loop(){
+if (BT.available()){     // si hay informacion disponible desde modulo
+  valor = BT.read();   // almacena en valor el caracter recibido desde modulo
+
+  if( valor == 'a' )   
+  {
+      digitalWrite(13,HIGH); 
+
+      Serial.println("encendido");
   }
+      
+  if( valor == 'b' )   
+   {
+      digitalWrite(13,LOW); 
+
+      Serial.println("apagado");
+   }
+      
+ 
 }
